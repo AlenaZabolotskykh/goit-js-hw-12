c115e51cb1365ba4283fefa93c2f4ee364c4de4d
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-07-15 18:14:23 +0300

    pagination

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import { getPicturesByQuery } from "./js/pixabay-api";
 const form = document.querySelector("form")
 const gallery = document.querySelector('.gallery')
 const loader = document.querySelector('.loader')
+const loadButton = document.querySelector('.button-load')
+const spinner = document.querySelector('.spinner')
+
+hide(loadButton);
+
 form.addEventListener('submit', handlerForm)
 
 const params = {
@@ -21,17 +26,29 @@ async function handlerForm (evt){
     gallery.innerHTML = "";
     params.page = 1; 
   params.q = form.elements.search.value.trim();
+
    if (!params.q) {
     iziToast.error({
       title: 'Рядок не може бути пустим',
     });
     return;}
 
+    show(loadButton);
+    disable(loadButton,loader);
+
     try{
-const {hits, total} = await getPicturesByQuery(params)
-console.log(hits, total)
+const {hits, total} = await getPicturesByQuery(params);
 params.maxPage = Math.ceil(total / params.pageSize)
-renderImage(hits)
+renderImage(hits);
+
+if(hits.length > 0 && hits.length !== total) {
+enable(loadButton, loader);
+loadButton.addEventListener('click', handleLoadMore)
+}
+else {
+  hide(loadButton);
+}
+
     }
     catch (err) {
 
@@ -47,4 +64,44 @@ form.reset()
 // .finally(() => {
 //   loader.style.display = 'none'
 // })
+}
+
+async function handleLoadMore(evt) {
+  params.page += 1;
+disable(loadButton, loader);
+
+try {
+  const {hits} = await getPicturesByQuery(params);
+renderImage(hits);
+}
+catch (err) {
+console.log(err);
+}
+finally {
+  enable(loadButton, loader) 
+if(params.page === params.maxPage) {
+  console.log("We're sorry, but you've reached the end of search results.");
+  hide(loadButton);
+  loadButton.removeEventListener('click', handleLoadMore);
+}
+}
+}
+
+
+function hide(loadButton){
+loadButton.classList.add('is-hidden');
+}
+
+function show (loadButton) {
+  loadButton.classList.remove('is-hidden');
+}
+
+function disable (loadButton, loader) {
+loadButton.disabled = true;
+        loader.style.display = 'block';
+}
+
+function enable(loadButton, loader) {
+  loadButton.disabled = false;
+        loader.style.display = 'none';
 }
